f50d2c7f5d8c2107a89e53e37cce7d96fc822a20
David W.H. Swenson <dwhs@hyperblazer.net>
2016-05-31 17:26:09 +0200

    Tests for mover (no asserts yet)

--- ops_piggybacker/tests/test_mover_stubs.py
+++ ops_piggybacker/tests/test_mover_stubs.py
@@ -15,15 +15,44 @@ class testShootingStub(object):
         # this instantiates an object, and ensures that instantiation works
         self.stub = oink.ShootingStub(common.tps_ensemble)
 
-    def test_forward_move(self):
-        # this checks that we get a reasonable PMC out of a given move
-        # TODO: check the return value in detail
-        pass
-
     def test_backward_move(self):
+        initial_sample = common.initial_tps_sample
+        move = common.tps_shooting_moves[0]
+        replica = move[0]
+        trial_trajectory = move[1]
+        shooting_point = move[2]
+        accepted = move[3]
+        self.stub.move(initial_sample, trial_trajectory, shooting_point,
+                       accepted)
         # TODO: check the return value in detail
-        pass
+        raise SkipTest
+
+    def test_forward_move(self):
+        init_traj = common.tps_shooting_moves[0][1]
+        initial_sample = paths.Sample(replica=0,
+                                      trajectory=init_traj,
+                                      ensemble=common.tps_ensemble)
+        move = common.tps_shooting_moves[1]
+        replica = move[0]
+        trial_trajectory = move[1]
+        shooting_point = move[2]
+        accepted = move[3]
+        self.stub.move(initial_sample, trial_trajectory, shooting_point,
+                       accepted)
+        # TODO: check the return value in detail
+        raise SkipTest
 
     def test_rejected_move(self):
+        init_traj = common.tps_shooting_moves[1][1]
+        initial_sample = paths.Sample(replica=0,
+                                      trajectory=init_traj,
+                                      ensemble=common.tps_ensemble)
+        move = common.tps_shooting_moves[2]
+        replica = move[0]
+        trial_trajectory = move[1]
+        shooting_point = move[2]
+        accepted = move[3]
+        self.stub.move(initial_sample, trial_trajectory, shooting_point,
+                       accepted)
         # TODO: check the return value in detail
-        pass
+        raise SkipTest
