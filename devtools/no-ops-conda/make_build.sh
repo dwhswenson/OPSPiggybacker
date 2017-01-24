64d6ca181c679ffc7389fbb752ed6df26460533f
David W.H. Swenson <dwhs@hyperblazer.net>
2017-01-24 17:15:57 +0100

    Attempt to fix build problem by pinning numpy

--- devtools/no-ops-conda/make_build.sh
+++ devtools/no-ops-conda/make_build.sh
@@ -3,13 +3,13 @@
 # Basic idea: get the requirements for OPS and put them in a file. Then have
 # conda install them.
 
-OPS_CONDA_RECIPE="https://raw.githubusercontent.com/choderalab/openpathsampling/master/devtools/conda-recipe"
+OPS_CONDA_RECIPE="https://raw.githubusercontent.com/openpathsampling/openpathsampling/master/devtools/conda-recipe"
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 curl ${OPS_CONDA_RECIPE}/meta.yaml | 
     grep "\ \ \ \ \-\ " | 
     sed 's/\ \ \ \ \-\ //' |
-    sed 's/\ .*//' |
+    #sed 's/\ .*//' |
     grep -v "openpathsampling" > ${MYDIR}/ops_reqs.txt
 
 cp ${MYDIR}/../conda-recipe/meta.yaml ${MYDIR}/meta.yaml
